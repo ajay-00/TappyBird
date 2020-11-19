ec567c0ac9a8b49376338ed777d872f649076d86
ajay-00 <58970472+ajay-00@users.noreply.github.com>
2020-11-19 10:27:48 -0500

    Add files via upload

---
+++ Assets/scripts/score.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class score : MonoBehaviour
+{
+    Text c;
+
+    void Start()
+    {
+        c = GetComponent<Text>();
+        c.text = "Score: " + GameManager.Instance.Score;
+    }
+}
